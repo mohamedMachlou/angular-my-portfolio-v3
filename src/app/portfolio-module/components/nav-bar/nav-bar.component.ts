59b9357fa20e12f8ed23900fe97d37f2983296c7
Mohamed Machlou <medmachlou@gmail.com>
2024-06-13 10:58:32 +0100

    editing Nav Bar icons and y services icons

--- src/app/portfolio-module/components/nav-bar/nav-bar.component.ts
+++ src/app/portfolio-module/components/nav-bar/nav-bar.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+  isActive = signal<String>('home');
+
+  goToHome() {
+    document.getElementById('home')?.scrollIntoView();
+    this.isActive.set('home');
+  }
+  goToService() {
+    document.getElementById('service')?.scrollIntoView();
+    this.isActive.set('service');
+  }
+  goToEducation() {
+    document.getElementById('education')?.scrollIntoView();
+    this.isActive.set('education');
+  }
+  goToPortfolio() {
+    document.getElementById('portfolio')?.scrollIntoView();
+    this.isActive.set('portfolio');
+  }
+  goToBlog() {
+    document.getElementById('blog')?.scrollIntoView();
+    this.isActive.set('blog');
+  }
+  goToContact() {
+    document.getElementById('contact')?.scrollIntoView();
+    this.isActive.set('contact');
+  }
+}
