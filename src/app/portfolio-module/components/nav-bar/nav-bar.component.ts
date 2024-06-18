75f6416dc82f08c606caca2d7fe0b30a672957a4
Mohamed Machlou <medmachlou@gmail.com>
2024-06-18 11:14:11 +0100

    blog and contact component HTML CSS with Functions on TS file

--- src/app/portfolio-module/components/nav-bar/nav-bar.component.ts
+++ src/app/portfolio-module/components/nav-bar/nav-bar.component.ts
@@ -8,28 +8,8 @@ import { Component, signal } from '@angular/core';
 export class NavBarComponent {
   isActive = signal<String>('home');
 
-  goToHome() {
-    document.getElementById('home')?.scrollIntoView();
-    this.isActive.set('home');
-  }
-  goToService() {
-    document.getElementById('service')?.scrollIntoView();
-    this.isActive.set('service');
-  }
-  goToEducation() {
-    document.getElementById('education')?.scrollIntoView();
-    this.isActive.set('education');
-  }
-  goToPortfolio() {
-    document.getElementById('portfolio')?.scrollIntoView();
-    this.isActive.set('portfolio');
-  }
-  goToBlog() {
-    document.getElementById('blog')?.scrollIntoView();
-    this.isActive.set('blog');
-  }
-  goToContact() {
-    document.getElementById('contact')?.scrollIntoView();
-    this.isActive.set('contact');
+  goToSection(name: string) {
+    document.getElementById(name)?.scrollIntoView();
+    this.isActive.set(name);
   }
 }
--- src/app/portfolio-module/components/portfolio/portfolio.component.ts
+++ src/app/portfolio-module/components/portfolio/portfolio.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'portfolio',
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css',
 })
-export class PortfolioComponent {}
+export class PortfolioComponent {
+  toStyle = signal<String>('all');
+
+  switchTo(name: string) {
+    this.toStyle.set(name);
+  }
+}
