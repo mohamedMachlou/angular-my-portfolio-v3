e79507d3251a0bbf28bef168930695669c62aa7b
Mohamed Machlou <medmachlou@gmail.com>
2024-06-20 19:20:30 +0100

    Up 6

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -30,6 +25,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-my-portfolio-v3');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, angular-my-portfolio-v3'
+    );
   });
 });
