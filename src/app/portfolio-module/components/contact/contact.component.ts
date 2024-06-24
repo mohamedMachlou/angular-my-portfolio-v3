bd15bbb4fa80b78e457b450e2b47ad112415894b
Mohamed Machlou <medmachlou@gmail.com>
2024-06-24 12:20:17 +0100

    npm run  Build

--- src/app/portfolio-module/components/contact/contact.component.ts
+++ src/app/portfolio-module/components/contact/contact.component.ts
@@ -9,29 +9,4 @@ export class ContactComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-
-  display: any;
-  center: google.maps.LatLngLiteral = {
-    lat: 30.4012,
-    lng: -9.58553,
-  };
-  zoom = 14;
-
-  /*------------------------------------------
-    --------------------------------------------
-    moveMap()
-    --------------------------------------------
-    --------------------------------------------*/
-  moveMap(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null) this.center = event.latLng.toJSON();
-  }
-
-  /*------------------------------------------
-    --------------------------------------------
-    move()
-    --------------------------------------------
-    --------------------------------------------*/
-  move(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null) this.display = event.latLng.toJSON();
-  }
 }
