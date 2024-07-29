515fe7fbfb3a2e8cb387f33f104ba690e8cdaae5
Mohamed Machlou <medmachlou@gmail.com>
2024-07-29 11:39:41 +0100

    editing switching contrast color for About Section

--- src/app/portfolio-module/components/nav-bar/nav-bar.component.ts
+++ src/app/portfolio-module/components/nav-bar/nav-bar.component.ts
@@ -24,17 +24,52 @@ export class NavBarComponent implements OnInit {
     this.switchClr.set(this.appearanceService.switchContrast());
 
     // Appearance Service
-    const body = document.querySelector('body');
-    const portfolio = document.querySelector('.portfolio');
-    const about = portfolio?.children[0];
     // const pers = document.querySelectorAll('.perso');
     // const perso = Array.from(pers);
-    const container = portfolio?.children[1];
-    const navbar = portfolio?.children[2]?.children[0]?.children[0];
+
+    /// Get Elements Back Body and Portfolio -- To switch color
+    const body = document.querySelector('body');
+    const portfolio = document.querySelector('.portfolio');
     const elemBack = [body, portfolio];
-    const elemFront = [about, container, navbar];
-    /// Get Elements div li  h2  h4 -- To switch color Black/white
-    const divs = Array.from(document.querySelectorAll('div'));
+
+    /// Get Elements Front  -- To switch color
+    const about = Array.from(document.querySelectorAll('#about'));
+    const blogs = Array.from(document.querySelectorAll('#blogs'));
+    const contact = Array.from(document.querySelectorAll('#contact'));
+    const education = Array.from(document.querySelectorAll('#education'));
+    const service = Array.from(document.querySelectorAll('#service'));
+    const navbar = Array.from(document.querySelectorAll('#navbar'));
+    const myportfolio = Array.from(document.querySelectorAll('#myportfolio'));
+    const price = Array.from(document.querySelectorAll('#price'));
+    const recommendation = Array.from(
+      document.querySelectorAll('#recommendation')
+    );
+    const welcome = Array.from(document.querySelectorAll('#welcome'));
+    const footer = Array.from(document.querySelectorAll('#footer'));
+
+    const frontElem = [
+      ...about,
+      ...blogs,
+      ...contact,
+      ...education,
+      ...service,
+      ...navbar,
+      ...myportfolio,
+      ...price,
+      ...recommendation,
+      ...welcome,
+      ...footer,
+    ];
+    console.log(frontElem);
+    // const navbar = portfolio?.children[2]?.children[0]?.children[0];
+    // const about = portfolio?.children[0];
+    // const elemFront = [about, navbar];
+
+    // Get Container's children
+    const containerChldrs = Array.from(portfolio?.children[1].children!);
+
+    /// Get Elements  li  h2  h4 -- To switch color Black/white
+    // const divs = Array.from(document.querySelectorAll('div'));
     const lists = Array.from(document.querySelectorAll('li'));
     const header2 = Array.from(document.querySelectorAll('h2'));
     const header4 = Array.from(document.querySelectorAll('h4'));
@@ -48,18 +83,28 @@ export class NavBarComponent implements OnInit {
     const paras = Array.from(document.querySelectorAll('p'));
     const gGG = [...header5, ...paras];
 
-    console.log(gGG);
-
     if (this.switchClr()) {
+      // Switch to White color for Elements  li  h2  h4
       gBW.map((ele) => {
         ele?.setAttribute('style', 'color: white');
       });
+
+      // Switch to Gray color for Elements  h5 p
       gGG.map((ele) => {
         ele?.setAttribute('style', 'color: var(--text-gray-contrastA)');
       });
-      elemFront.map((ele) => {
-        ele?.setAttribute('style', 'background-color :var(--main-back-color)');
+
+      // Switch to Black color All FrontElem
+      frontElem.map((ele) => {
+        ele?.setAttribute(
+          'style',
+          'background-color :var(--front-color-After)'
+        );
       });
+
+      // Switch to Black color for Container Childrens
+
+      // Switch to Gray color for body and portfolio◘
       elemBack.map((ele) => {
         ele?.setAttribute(
           'style',
@@ -67,15 +112,30 @@ export class NavBarComponent implements OnInit {
         );
       });
     } else {
+      // Switch to Black color for Elements  li  h2  h4
       gBW.map((ele) => {
         ele?.setAttribute('style', 'color: black');
       });
+
+      // Switch to Gray color for Elements  h5 p
       gGG.map((ele) => {
         ele?.setAttribute('style', 'color: Black');
       });
-      elemFront.map((ele) => {
-        ele?.setAttribute('style', 'background-color :  white');
+
+      // Switch to White color All FrontElem
+      frontElem.map((ele) => {
+        ele?.setAttribute(
+          'style',
+          'background-color :var(--front-color-Befor)'
+        );
       });
+
+      // Switch to White color for Container Childrens
+      containerChldrs.map((ele) => {
+        ele?.setAttribute('style', 'background-color : white');
+      });
+
+      // Switch to Gray color for body and portfolio
       elemBack.map((ele) => {
         ele?.setAttribute(
           'style',
