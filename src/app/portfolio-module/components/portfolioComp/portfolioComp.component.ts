2a897238cbef037a329951c4ef75c5e4683f98ad
Mohamed Machlou <medmachlou@gmail.com>
2024-08-02 09:03:56 +0100

    start slider

--- src/app/portfolio-module/components/portfolioComp/portfolioComp.component.ts
+++ src/app/portfolio-module/components/portfolioComp/portfolioComp.component.ts
@@ -9,11 +9,14 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
   imgToShow: any[] = [];
+  prevImg = signal<string>('');
+  mainImg = signal<string>('');
+  nextImg = signal<string>('');
   // Injections Services
   manageListsService = inject(ManageListsService);
   showPotfolioImgService = inject(ShowPotfolioImgService);
 
-  // set current ID  method
+  // set current ID method
   setId(id: number) {
     this.manageListsService.manageListById(id);
   }
@@ -24,25 +27,18 @@ export class PortfolioComponent implements OnInit {
     this.imgToShow = this.showPotfolioImgService.showImgfromService;
   }
 
-  // //show Design Images
-  // show_design_img() {
-  //   this.showImg = [...this.design_img];
-  // }
-  // //show Web Templates Images
-  // show_web_tem_img() {
-  //   this.showImg = [...this.web_templates_img];
-  // }
-  // //show  Web Apps Images
-  // show_web_apps_img() {
-  //   this.showImg = [...this.web_apps_img];
-  // }
-  // //show Logo Images
-  // show_logo_img() {
-  //   this.showImg = [...this.logo_img];
-  // }
+  //// Slider
+  slider() {
+    const slide = document.querySelectorAll('slide');
+    console.log(slide);
+    this.prevImg.set(this.imgToShow[0]);
+    this.mainImg.set(this.imgToShow[1]);
+    this.nextImg.set(this.imgToShow[2]);
+  }
 
   ngOnInit(): void {
     this.setId(0);
     this.getImages();
+    this.slider();
   }
 }
